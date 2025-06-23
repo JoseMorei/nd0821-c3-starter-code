688b5d236fdb779f6415c863bc1fe87c705886aa
jose moreira <jose.moreira@sysdig.com>
2025-06-23 16:00:12 -0700

    Trigger GH Action

--- starter/starter/test_main.py
+++ starter/starter/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app
+from main import app, InputData 
 
 client = TestClient(app)
 
@@ -9,47 +9,46 @@ def test_root():
     assert res.status_code == 200
     assert res.json()["message"] == "Welcome to the Income Prediction API!"
 
-
 # POST test - over 50K 
-# def test_predict_over_50k():
-#     input_data = {
-#         "age": 52,
-#         "workclass": "Self-emp-not-inc",
-#         "fnlwgt": 209642,
-#         "education": "HS-grad",
-#         "education-num": 9,
-#         "marital-status": "Married-civ-spouse",
-#         "occupation": "Exec-managerial",
-#         "relationship": "Husband",
-#         "race": "White",
-#         "sex": "Male",
-#         "capital-gain": 0,
-#         "capital-loss": 0,
-#         "hours-per-week": 45,
-#         "native-country": "United-States"
-#     }
-#     res = client.post("/predict", json=input_data)
-#     assert res.status_code == 200
-#     assert res.json()["prediction"] in [0, 1]
+def test_predict_over_50k():
+    input_data = InputData(
+        age=52,
+        workclass="Self-emp-not-inc",
+        fnlwgt=209642,
+        education="HS-grad",
+        education_num=9,
+        marital_status="Married-civ-spouse",
+        occupation="Exec-managerial",
+        relationship="Husband",
+        race="White",
+        sex="Male",
+        capital_gain=0,
+        capital_loss=0,
+        hours_per_week=45,
+        native_country="United-States"
+    )
+    res = client.post("/predict", json=input_data.dict(by_alias=True))
+    assert res.status_code == 200
+    assert res.json()["prediction"] in [0, 1]
     
 # # POST test - under 50K 
-# def test_predict_under_50k():
-#     input_data = {
-#         "age": 23,
-#         "workclass": "Private",
-#         "fnlwgt": 190709,
-#         "education": "Some-college",
-#         "education-num": 10,
-#         "marital-status": "Never-married",
-#         "occupation": "Other-service",
-#         "relationship": "Own-child",
-#         "race": "Black",
-#         "sex": "Female",
-#         "capital-gain": 0,
-#         "capital-loss": 0,
-#         "hours-per-week": 20,
-#         "native-country": "United-States"
-#     }
-#     res = client.post("/predict", json=input_data)
-#     assert res.status_code == 200
-#     assert res.json()["prediction"] in [0, 1]
+def test_predict_under_50k():
+    input_data = InputData(
+        age= 23,
+        workclass= "Private",
+        fnlwgt= 190709,
+        education= "Some-college",
+        education_num= 10,
+        marital_status= "Never-married",
+        occupation= "Other-service",
+        relationship= "Own-child",
+        race= "Black",
+        sex= "Female",
+        capital_gain=0,
+        capital_loss=0,
+        hours_per_week= 20,
+        native_country= "United-States"
+    )
+    res = client.post("/predict", json=input_data.dict(by_alias=True))
+    assert res.status_code == 200
+    assert res.json()["prediction"] in [0, 1]
